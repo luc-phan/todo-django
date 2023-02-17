211c468d834eb7cd473c1ed230a32d2c45be2ba9
Luc PHAN <phanthienluc@gmail.com>
2023-02-17 14:24:30 +0100

    feat: add detail view

--- todo_website/todo_crud/models.py
+++ todo_website/todo_crud/models.py
@@ -6,3 +6,4 @@ from django.db import models
 class Todo(models.Model):
     done = models.BooleanField()
     title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, null=True)
