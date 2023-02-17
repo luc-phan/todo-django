1b2a88d56aa3614d732813a059243586f5f13897
Luc PHAN <phanthienluc@gmail.com>
2023-02-17 15:09:11 +0100

    feat: add task creation

--- todo_website/todo_crud/forms.py
+++ todo_website/todo_crud/forms.py
@@ -3,9 +3,19 @@ from django import forms
 from .models import Todo
 
 
-class TodoForm(forms.ModelForm):
+class IndexTodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ["done", "title"]
 
+
 TodoFormSet = forms.modelformset_factory(Todo, fields=["done"], extra=0)
+
+
+class NewTodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ["done", "title", "description"]
+        widgets = {
+            "description": forms.Textarea()
+        }
