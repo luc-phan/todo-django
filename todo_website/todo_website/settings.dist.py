362472e3115d626bc49b2f0efe45a574330c445a
Luc PHAN <phanthienluc@gmail.com>
2023-02-18 09:56:26 +0100

    fix: add app to settings

--- todo_website/todo_website/settings.dist.py
+++ todo_website/todo_website/settings.dist.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'todo_crud.apps.TodoCrudConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
