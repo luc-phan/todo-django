97e5af4af1fbef3ce1692b05d4e614939bc64885
Luc PHAN <luc-phan@users.noreply.github.com>
2023-02-19 12:42:43 +0100

    feat: add bootstrap

--- todo_website/todo_crud/forms.py
+++ todo_website/todo_crud/forms.py
@@ -3,13 +3,14 @@ from django import forms
 from .models import Todo
 
 
-class IndexTodoForm(forms.ModelForm):
-    class Meta:
-        model = Todo
-        fields = ["done", "title"]
-
-
-TodoFormSet = forms.modelformset_factory(Todo, fields=["done"], extra=0)
+TodoFormSet = forms.modelformset_factory(
+    Todo,
+    fields=["done"],
+    extra=0,
+    widgets = {
+        'done': forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    }
+)
 
 
 class NewTodoForm(forms.ModelForm):
@@ -17,5 +18,6 @@ class NewTodoForm(forms.ModelForm):
         model = Todo
         fields = ["done", "title", "description"]
         widgets = {
-            "description": forms.Textarea()
+            "title": forms.TextInput(attrs={'class': "form-control"}),
+            "description": forms.Textarea(attrs={'class': "form-control"})
         }
